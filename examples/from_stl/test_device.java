6fb0f032d907710547a0eb9647457b0ff3607f4e
Adam Frees <adamfrees@Adams-MacBook-Pro.local>
2019-05-01 11:40:42 -0500

    updated examples

--- examples/from_stl/test_device.java
+++ examples/from_stl/test_device.java
@@ -8,6 +8,10 @@ public class test_device {
       run();
    }
    public static Model run() {
+     ComsolMaterial si = new ComsolMaterial("Si",11.7);
+     ComsolMaterial sige = new ComsolMaterial("SiGe",13.19);
+     ComsolMaterial alO2 = new ComsolMaterial("AlO2",9.);
+     ComsolMaterial al = new ComsolMaterial("Al",1.);
       Double xDims = 1000.;
       Double yDims = 1000.;
       ComsolBuilder builder = new ComsolBuilder(xDims,yDims);
@@ -22,24 +26,10 @@ public class test_device {
 
       builder.model.geom("geom1").run("fin");
 
-
-      builder.model.component("comp1").material().create("Si", "Common");
-      builder.model.component("comp1").material("Si").selection().named("geom1_SiWell_dom");
-      builder.model.component("comp1").material("Si").propertyGroup("def").set("relpermittivity", new String[]{"11.7"});
-
-
-      builder.model.component("comp1").material().create("SiGe", "Common");
-      builder.model.component("comp1").material("SiGe").selection().named("geom1_SiGeSel_dom");
-      builder.model.component("comp1").material("SiGe").propertyGroup("def").set("relpermittivity", new String[]{"13.19"});
-
-      builder.model.component("comp1").material().create("AlO2", "Common");
-      builder.model.component("comp1").material("AlO2").selection().named("geom1_Oxide_dom");
-      builder.model.component("comp1").material("AlO2").propertyGroup("def").set("relpermittivity", new String[]{"9."});
-
-      builder.model.component("comp1").material().create("Al", "Common");
-      builder.model.component("comp1").material("Al").selection().named("geom1_ElectrodeSel_dom");
-      builder.model.component("comp1").material("Al").propertyGroup("def").set("relpermittivity", new String[]{"1."});
-
+      builder.addMaterial(si,"geom1_SiWell_dom");
+      builder.addMaterial(sige,"geom1_SiGeSel_dom");
+      builder.addMaterial(alO2,"geom1_Oxide_dom");
+      builder.addMaterial(al,"geom1_ElectrodeSel_dom");
 
       builder.model.physics().create("es", "Electrostatics", "geom1");
 
