20f181393068d56503f39f6a2fa4d2fb1d00bc9b
Adam Frees <adamfrees@Adams-MacBook-Pro.local>
2019-05-01 09:42:36 -0500

    class starting point

---
+++ src/Material.java
@@ -0,0 +1,12 @@
+package comsolbuilder;
+
+public class Material {
+  public String name;
+  public Double relpermittivity;
+
+  public Material(String nameInput, Double relpermittivityInput){
+    name = nameInput;
+    relpermittivity = relpermittivityInput;
+  }
+
+}
